963dbb40fc38e232c7cc8f1698c29bbd75103381
xiaoyang <545955020@qq.com>
2017-06-16 15:09:47 +0800

    xxx

---
+++ test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test
+//
+//  Created by arno on 2017/6/16.
+//  Copyright © 2017年 520it.com.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
